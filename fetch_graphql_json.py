f57e0ed9a4fb2e0c19e8477f9741258cd99ed901
deytalytics <james.dey@shell.com>
2023-01-06 11:42:47 +0000

    Added ability to test basic authentication of a data product

--- fetch_graphql_json.py
+++ fetch_graphql_json.py
@@ -2,7 +2,7 @@ from python_graphql_client import GraphqlClient
 
 def fetch_graphql_json(variables):
     # Instantiate the client with an endpoint.
-    client = GraphqlClient(endpoint="https://dp-poc.azurewebsites.net/graphql")
+    client = GraphqlClient(endpoint="http://127.0.0.1/graphql")
 
     # Create the query string and variables required for the request.
     query = """
--- fetch_restapi_json.py
+++ fetch_restapi_json.py
@@ -1,7 +1,22 @@
 #Retrieve the continents & countries data from the REST API
-import requests
+import requests, json
+from base64 import b64encode
+# Authorization token: we need to base 64 encode it
+# and then decode it to acsii as python 3 stores it as a byte string
+def basic_auth(username, password):
+    token = b64encode(f"{username}:{password}".encode('utf-8')).decode("ascii")
+    return f'Basic {token}'
 
+username = "scott"
+password = "*****"
 def fetch_restapi_json():
-    response = requests.get('https://dp-poc.azurewebsites.net/REST/0.1/countries')
-    json_data = response.json() if response and response.status_code == 200 else {"Error": "REST API did not return any data"}
-    return json_data
+    headers = {'Authorization': basic_auth(username, password)}
+    try:
+        response = requests.get('http://127.0.0.1:80/REST/0.1/countries', headers=headers)
+        response.raise_for_status()
+        if response and response.status_code == 200:
+            json_data = response.json()
+            return json_data
+    except requests.exceptions.HTTPError as e:
+        response_text = json.loads(e.response.text)
+        return {'Error':str(response.status_code)+' '+response.reason+' - '+response_text['detail']}
