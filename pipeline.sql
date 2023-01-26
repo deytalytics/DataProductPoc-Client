e878c32d70216f5e82b15db03bca23877a10c110
deytalytics <james.dey@shell.com>
2023-01-26 17:09:31 +0000

    Made some changes due to testing implementation of versioning

--- pipeline.sql
+++ pipeline.sql
@@ -1,3 +1,4 @@
-# continents_and_countries
-select * from continents cn, countries c
+# continents_and_countries 0.1
+select cn.ContinentName,cn.ContinentCode, c.CountryName, c.TwoLetterCountryCode, c.ThreeLetterCountryCode,c.CountryNumber
+from continents cn, countries c
 where c.ContinentCode = cn.ContinentCode;
