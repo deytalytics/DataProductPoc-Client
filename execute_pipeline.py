3b0ffc572a12a4868bee77de399405113126ce5f
deytalytics <james_dey@hotmail.com>
2023-03-05 16:13:01 +0000

    Changed pipeline execution to a POST

--- execute_pipeline.py
+++ execute_pipeline.py
@@ -1,16 +1,13 @@
-#Retrieve the continents & countries data from the REST API
-import requests, pandas as pd, json
-from io import StringIO
 from authentication import basic_auth
+import requests, json
 
-def fetch_restapi_csv(url, username, password):
+def execute_pipeline(url, username, password):
     headers = {'Authorization': basic_auth(username, password)}
     try:
-        response = requests.get(url, headers=headers, params={'format':'csv'})
+        response = requests.post(url, headers=headers)
         response.raise_for_status()
         if response and response.status_code == 200:
-            df = pd.read_csv(StringIO(response.text))
-            return df
+            return {'Details':'pipeline.sql has been successfully executed'}
     except requests.exceptions.HTTPError as e:
         response_text = json.loads(e.response.text)
         return {'Error':str(response.status_code)+' '+response.reason+' - '+response_text['detail']}
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from fetch_restapi_csv import fetch_restapi_csv
 from fetch_restapi_schema import fetch_restapi_schema
 from fetch_restapi_docs import fetch_restapi_docs
 from post_data_file import post_file
+from execute_pipeline import execute_pipeline
 
 prod_url = 'https://t-and-s-dp-poc.azurewebsites.net/'
 prod_tmp_url='https://data-container.azurewebsites.net/'
-dev_url = 'http://127.0.0.1/'
-url = prod_tmp_url
+dev_url = 'http://127.0.0.1:8000/'
+url = dev_url
 
 print("\nLogging in as R2D2 (who is authorised to upload files)")
 #Test pushing countries and continents files to REST API input data port
@@ -19,9 +20,14 @@ print(response)
 response = post_file(url+'uploadsrcfile', 'continents.csv', username, password)
 print(response)
 
-#Test executing a pipeline to create target dataset from source dataset
+#Test uploading the pipeline.sql file
+print("\nUploading pipeline.sql to the rest api")
+response = post_file(url+'uploadpipeline', 'pipeline.sql', username, password)
+print(response)
+
+#Test executing the pipeline.sql file
 print("\nExecute the pipeline which will transform source data into an abstraction layer and push to target dataset")
-response = post_file(url+'executepipeline', 'pipeline.sql', username, password)
+response = execute_pipeline(url+'executepipeline', username, password)
 print(response)
 
 #Lets try and login as R2D2 and fetch REST API in JSON format
@@ -29,7 +35,7 @@ print("\nLogging in as R2D2 (who isn't allowed to view the dataset)")
 username = "R2D2"
 password = "tiger"
 print("\nFetching continents & countries data from the REST API output data port with an authenticated but unauthorised user")
-response = fetch_restapi_json(url+'continents_and_countries',username,password)
+response = fetch_restapi_json(url+'ds/continents_and_countries',username,password)
 print(response)
 
 #Lets try and login as Mary with wrong password and fetch REST API in JSON format
@@ -37,7 +43,7 @@ print("\nLogging in as Mary (who is allowed to view the dataset) but with wrong 
 username = "Mary"
 password = "tier"
 print("\nFetching continents & countries data from the REST API output data port with an unauthenticated user")
-response = fetch_restapi_json(url+'continents_and_countries',username,password)
+response = fetch_restapi_json(url+'ds/continents_and_countries',username,password)
 print(response)
 
 #Fetch all continents & countries data from our REST API in JSON format
@@ -45,12 +51,12 @@ print("\nLogging in as Mary with correct password")
 username = "Mary"
 password = "tiger"
 print("\nFetching continents & countries data from the REST API output data port with an authenticated and authorised user")
-json_data = fetch_restapi_json(url+'continents_and_countries',username,password)
+json_data = fetch_restapi_json(url+'ds/continents_and_countries',username,password)
 print(json_data)
 
 #Fetch all continents & countries data from our REST API in CSV format into a dataframe
 print("\nFetching continents & countries data from the REST API output data port in CSV format")
-df = fetch_restapi_csv(url+'continents_and_countries',username,password)
+df = fetch_restapi_csv(url+'ds/continents_and_countries',username,password)
 print(df)
 
 
@@ -59,14 +65,14 @@ print("\nLogging back in as Mary")
 username = "Mary"
 password = "tiger"
 print("\nFetch latest version of continents & countries data from our REST API in JSON format")
-json_data = fetch_restapi_json(url+'continents_and_countries',username,password)
+json_data = fetch_restapi_json(url+'ds/continents_and_countries',username,password)
 print(json_data)
 
 #Fetch original version of continents & countries data from our REST API in JSON format
 username = "Mary"
 password = "tiger"
 print("\nFetch original version of continents & countries data from our REST API in JSON format")
-json_data = fetch_restapi_json(url+'continents_and_countries/0.1',username,password)
+json_data = fetch_restapi_json(url+'ds/continents_and_countries/0.1',username,password)
 print(json_data)
 
 #Fetch the data dictionary for the continents and countries dataset
